c26e0456a1d894bda40956d75f1a8080e5cdf71a
SomebodyAroundThere <156821622+SomebodyAroundThere@users.noreply.github.com>
2024-02-12 12:55:34 +0300

    создание функции чнения файлы построчно

--- bf.py
+++ bf.py
@@ -1,6 +1,8 @@
 
 
-def read_lines():
-    print("Hello world")
+def read_lines(filename: str):
+    with open(file=filename, mode='r') as file:
+         while content := file.readline():
+            yield content
 def do_bruteforce():
     print("Hello world")
