ee53cb69ca5a2e4f9b43b813911a263efeda45dc
P0m1dorka <loginovandrej31@gmail.com>
2024-02-20 13:00:20 +0300

    pivo

--- bf.py
+++ bf.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 
 
 def read_lines(filename: str):
     with open(file=filename, mode='r') as file:
          while content := file.readline():
             yield content
-=======
-def read_lines():
-    print("Hello world")
->>>>>>> a7192e2 (pivo)
 def do_bruteforce():
     print("Hello world")
